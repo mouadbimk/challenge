6386f9c7977060615a9dd94b2cab46c8bc3fa6fe
Mouad <mouadbimekliouen@gmail.com>
2025-02-03 16:40:29 +0100

    work in last article

--- project 1/app/js/app.js
+++ project 1/app/js/app.js
@@ -2,17 +2,28 @@ document.addEventListener("DOMContentLoaded", function(){
     const BtnHamburger = () =>{
         const btnHamburger = document.querySelector('#btnHamburger');
         const header = document.querySelector('.header');
-        const overlay = document.querySelector('.overlay');
+        const body = document.querySelector('body');
+        const fadeElements = document.querySelectorAll('.has-fade');
         if(!btnHamburger) return;
         btnHamburger.addEventListener('click', function(){
             if(header.classList.contains('open')){
+                body.classList.remove('noscroll')
                 header.classList.remove('open');
-                overlay.classList.remove('fade-in');
-                overlay.classList.add('fade-out');
+                fadeElements.forEach(function(element){
+                    element.classList.remove('fade-in');
+                    element.classList.add('fade-out');
+                });
+               
+
+
             }else{
+                body.classList.add('noscroll')
                 header.classList.add('open');
-                overlay.classList.remove('fade-out');
-                overlay.classList.add('fade-in');
+                fadeElements.forEach(function(element){
+                    element.classList.add('fade-in');
+                    element.classList.remove('fade-out');
+                });
+               
             }
            
         });
