3e3fa162c24c1094a0b6f0bba876ef5a195e80d3
Mouad Bimekliouen <mouadbimekliouen@gmail.com>
2025-02-03 08:14:48 +0100

    work in menu mobile

--- project 1/app/js/app.js
+++ project 1/app/js/app.js
@@ -2,12 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
     const BtnHamburger = () =>{
         const btnHamburger = document.querySelector('#btnHamburger');
         const header = document.querySelector('.header');
+        const overlay = document.querySelector('.overlay');
         if(!btnHamburger) return;
         btnHamburger.addEventListener('click', function(){
             if(header.classList.contains('open')){
-                header.classList.remove('open')
+                header.classList.remove('open');
+                overlay.classList.remove('fade-in');
+                overlay.classList.add('fade-out');
             }else{
-                header.classList.add('open')
+                header.classList.add('open');
+                overlay.classList.remove('fade-out');
+                overlay.classList.add('fade-in');
             }
            
         });
